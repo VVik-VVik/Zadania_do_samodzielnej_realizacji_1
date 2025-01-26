75d8c96ba000645282171633b5c464d3501785a2
VVik-VVik <miriadinna@gmail.com>
2025-01-26 12:09:43 +0100

    Create lekcie 26,01,2025-zadania2.sql

---
+++ lekcie 26,01,2025-zadania2.sql
@@ -0,0 +1,82 @@
+-- 1. Wyświetl wszystkich użytkowników wraz z ich zamówieniami.
+
+-- poprawnie
+SELECT Uzytkownicy.imie, Uzytkownicy.nazwisko, Zamowienia.data_Zamowienia, Zamowienia.ilosc
+FROM Uzytkownicy
+JOIN Zamowienia ON Uzytkownicy.id=Zamowienia.uzytkownik_id;
+-- 2. Wyświetl wszystkie zamówienia zawierające produkty.
+SELECT Produkty.nazwa, Zamowienia.produkt_id
+FROM Produkty
+JOIN Zamowienia ON Produkty.id=Zamowienia.produkt_id;
+-- poprawnie
+
+-- 3. Wyświetl imię i nazwisko klientów, którzy zamówili produkty z kategorii 'Elektronika'.
+SELECT Uzytkownicy.imie, Uzytkownicy.nazwisko, Produkty.kategoria
+FROM Uzytkownicy
+JOIN Zamowienia ON Uzytkownicy.id=Zamowienia.uzytkownik_id
+JOIN Produkty ON Produkty.id=Zamowienia.produkt_id
+WHERE Produkty.kategoria LIKE 'Elektronika';
+-- poprawnie
+
+-- 4. Uzyskaj nazwę produktu, datę zamowienia oraz ilość.
+SELECT Produkty.nazwa, Zamowienia.data_zamowienia, Zamowienia.ilosc
+FROM Produkty
+JOIN Zamowienia ON Produkty.id=Zamowienia.produkt_id;
+-- poprawnie
+
+-- 5. Wyświetl pełną listę zamówień, uwzględniając tylko tych użytkowników, którzy dokonali zamówienia.
+SELECT Zamowienia.data_zamowienia, Zamowienia.ilosc, Uzytkownicy.imie, Uzytkownicy.nazwisko
+FROM Uzytkownicy
+JOIN Zamowienia ON Uzytkownicy.id=Zamowienia.uzytkownik_id;
+-- poprawnie
+
+-- 6. Wyświetl zamówione produkty oraz ich cenę.
+SELECT Produkty.nazwa, Produkty.cena
+FROM Produkty
+JOIN Zamowienia ON Produkty.id=Zamowienia.produkt_id;
+-- poprawnie
+
+-- 7. Wyświetl imię, nazwisko oraz produkty, które zostały zamówione.
+SELECT Uzytkownicy.imie, Uzytkownicy.nazwisko, Produkty.nazwa
+FROM Uzytkownicy
+JOIN Zamowienia ON Uzytkownicy.id=Zamowienia.uzytkownik_id
+JOIN Produkty ON Produkty.id=Zamowienia.produkt_id;
+-- poprawnie
+
+-- 8. Połącz tabelę Zamowienia i Uzytkownicy, aby wyświetlić użytkowników oraz daty ich zamówień.
+SELECT Zamowienia.data_zamowienia, Uzytkownicy.imie, Uzytkownicy.nazwisko
+FROM Uzytkownicy
+JOIN Zamowienia ON Uzytkownicy.id=Zamowienia.uzytkownik_id;
+-- poprawnie
+
+-- 9. Wyświetl użytkowników oraz produkty, które zamówili, posortowane po dacie zamówienia.
+SELECT Zamowienia.data_zamowienia, Uzytkownicy.imie, Uzytkownicy.nazwisko, Produkty.nazwa
+FROM Uzytkownicy
+JOIN Zamowienia ON Uzytkownicy.id=Zamowienia.uzytkownik_id
+JOIN Produkty ON Produkty.id=Zamowienia.produkt_id 
+ORDER BY Zamowienia.data_zamowienia ASC;
+-- poprawnie
+
+-- 10. Połącz tabelę Produkty i Zamowienia, aby uzyskać listę produktów zamówionych w dniu 1 stycznia 2025.
+SELECT Zamowienia.data_zamowienia, Zamowienia.ilosc, Produkty.nazwa, Produkty.cena, Produkty.kategoria
+FROM Produkty
+JOIN Zamowienia ON Produkty.id=Zamowienia.produkt_id
+WHERE Zamowienia.data_zamowienia = '2025-01-01';
+-- poprawnie
+
+-- 11. Połącz tabelę Uzytkownicy i Produkty, aby wyświetlić imiona i nazwiska użytkowników oraz produkty, które zamówili w kategorii 'Meble'.
+SELECT Uzytkownicy.imie, Uzytkownicy.nazwisko, Produkty.nazwa
+FROM Uzytkownicy
+JOIN Zamowienia ON Uzytkownicy.id=Zamowienia.uzytkownik_id
+JOIN Produkty ON Produkty.id=Zamowienia.produkt_id
+WHERE Produkty.kategoria LIKE 'Meble';
+-- poprawnie
+
+-- 12. Połącz tabelę Produkty i Zamowienia, aby uzyskać produkty, które zostały zamówione więcej niż 3 razy.
+
+-- poprawnie
+SELECT Produkty.nazwa, SUM(Zamowienia.ilosc) AS total_ilosc
+FROM Produkty
+JOIN Zamowienia ON Produkty.id=Zamowienia.produkt_id
+GROUP BY  Produkty.nazwa
+HAVING total_ilosc > 3;
